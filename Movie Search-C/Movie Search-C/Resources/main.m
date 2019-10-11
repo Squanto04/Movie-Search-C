2385256a42fccffe878fe6c07173d18feec6ffab
Jordan Lamb <squanto@Jordans-MacBook-Pro.local>
2019-10-11 09:42:12 -0600

    Xcode added and organized. Storyboard Layout Complete

---
+++ Movie Search-C/Movie Search-C/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Movie Search-C
+//
+//  Created by Jordan Lamb on 10/11/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Movie Search-C/Movie Search-C/Sources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie Search-C
+//
+//  Created by Jordan Lamb on 10/11/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
